6b530b143d7c6120b27e4fb52250dbbe2c15f705
Sihoo-milk <>
2020-11-07 17:38:52 +0900

    googleタグマネージャータグの追加

--- contact/index.php
+++ contact/index.php
@@ -127,6 +127,10 @@ function validation($data) {
   <script type="text/javascript" src="/js/common.js" defer></script>
 </head>
 <body>
+  <!-- Google Tag Manager (noscript) -->
+  <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WBLZ3KD"
+  height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+  <!-- End Google Tag Manager (noscript) -->
   <div class="wrapper">
     <header class="header">
       <div class="header__inner">
