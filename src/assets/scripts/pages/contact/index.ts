9ca419ac43917daba89b2a021744afa5dd7b3b30
tomohiro.maeno <>
2025-09-03 23:41:58 +0700

    change: Install Vite and TypeScript.

---
+++ src/assets/scripts/pages/contact/index.ts
@@ -0,0 +1,2 @@
+import '../../../styles/ress.scss';
+import '../../../styles/pages/contact/index.scss';
---
+++ src/assets/scripts/pages/expert/index.ts
@@ -0,0 +1,27 @@
+import '../../../styles/ress.scss';
+import '../../../styles/pages/expert/index.scss';
+
+/* ========================================================
+タブメニュー
+=========================================================*/
+const tabTriggers = document.querySelectorAll('.js-tab-trigger');
+const tabTargets = document.querySelectorAll('.js-tab-target');
+
+tabTriggers.forEach((trigger) => {
+  trigger.addEventListener('click', (e) => {
+    const currentMenu = e.currentTarget as HTMLElement | null;
+    if (!currentMenu) return;
+
+    const currentContent = currentMenu.dataset.id
+      ? document.getElementById(currentMenu.dataset.id)
+      : null;
+
+    tabTriggers.forEach((t) => t.classList.remove('is-active'));
+    currentMenu.classList.add('is-active');
+
+    tabTargets.forEach((t) => t.classList.remove('is-active'));
+    if (currentContent) {
+      currentContent.classList.add('is-active');
+    }
+  });
+});
