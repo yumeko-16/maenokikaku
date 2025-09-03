18c5ec605e278e8592a61301716382afbb6a1c84
tomohiro.maeno <>
2025-09-04 00:12:12 +0700

    clean: Reformat.

--- dist/contact/index.php
+++ dist/contact/index.php
@@ -1,110 +1,105 @@
 <?php
-// 変数の初期化
-$page_flag = 0;
-$clean = array();
-$error = array();
-
-// サニタイズ
-if( !empty($_POST) ) {
-	foreach( $_POST as $key => $value ) {
-		$clean[$key] = htmlspecialchars( $value, ENT_QUOTES );
-	}
-}
-
-if( !empty($clean['btn_confirm']) ) {
-
-  $error = validation($clean);
-
-  if( empty($error) ) {
-    $page_flag = 1;
-
-    // セッションの書き込み
-		session_start();
-		$_SESSION['page'] = true;
-  }
-
-} elseif ( !empty($clean['btn_submit']) ) {
-
-  session_start();
-	if( !empty($_SESSION['page']) && $_SESSION['page'] === true ) {
-
-		// セッションの削除
-		unset($_SESSION['page']);
-  $page_flag = 2;
-
-  // 変数とタイムゾーンを初期化
-  $header = null;
-	$auto_reply_subject = null;
-	$auto_reply_text = null;
-  $admin_reply_subject = null;
-	$admin_reply_text = null;
-	date_default_timezone_set('Asia/Tokyo');
-
-  // ヘッダー情報を設定
-	$header = "MIME-Version: 1.0\n";
-	$header .= "From: 株式会社前野企画 <maenokikaku@email.plala.or.jp>\n";
-	$header .= "Reply-To: 株式会社前野企画 <maenokikaku@email.plala.or.jp>\n";
-
-  // 件名を設定
-  $auto_reply_subject = 'お問い合わせありがとうございます。';
-
-  // 本文を設定
-  $auto_reply_text = "この度は、お問い合わせ頂き誠にありがとうございます。
-下記の内容でお問い合わせを受け付けました。\n\n";
-	$auto_reply_text .= "お問い合わせ日時：" . date("Y-m-d H:i") . "\n";
-	$auto_reply_text .= "氏名：" . $clean['your_name'] . "\n";
-	$auto_reply_text .= "メールアドレス：" . $clean['email'] . "\n\n";
-	$auto_reply_text .= "電話番号：" . $clean['tel'] . "\n\n";
-  $auto_reply_text .= "お問い合わせ内容：" . nl2br($clean['contact']) . "\n\n";
-  $auto_reply_text .= "株式会社前野企画";
-
-  // メール送信
-  mb_send_mail( $clean['email'], $auto_reply_subject, $auto_reply_text, $header);
-
-  // 運営側へ送るメールの件名
-	$admin_reply_subject = "お問い合わせを受け付けました";
-
-	// 本文を設定
-	$admin_reply_text = "下記の内容でお問い合わせがありました。\n\n";
-	$admin_reply_text .= "お問い合わせ日時：" . date("Y-m-d H:i") . "\n";
-	$admin_reply_text .= "氏名：" . $clean['your_name'] . "\n";
-  $admin_reply_text .= "メールアドレス：" . $clean['email'] . "\n\n";
-  $admin_reply_text .= "電話番号：" . $clean['tel'] . "\n\n";
-  $admin_reply_text .= "お問い合わせ内容：" . nl2br($clean['contact']) . "\n\n";
-
-	// 運営側へメール送信
-  mb_send_mail( 'maenokikaku@email.plala.or.jp', $admin_reply_subject, $admin_reply_text, $header);
-
-  } else {
-		$page_flag = 0;
-	}
-}
-
-function validation($data) {
-
+  // 変数の初期化
+  $page_flag = 0;
+  $clean = array();
   $error = array();
 
-  // 氏名のバリデーション
-  if( empty($data['your_name']) ) {
-    $error[] = "「氏名」は必ず入力してください。";
-  } elseif( 20 < mb_strlen($data['your_name']) ) {
-		$error[] = "「氏名」は20文字以内で入力してください。";
-	}
+  // サニタイズ
+  if (!empty($_POST)) {
+    foreach($_POST as $key => $value) {
+      $clean[$key] = htmlspecialchars( $value, ENT_QUOTES );
+    }
+  }
 
-  // メールアドレスのバリデーション
-	if( empty($data['email']) ) {
-		$error[] = "「メールアドレス」は必ず入力してください。";
-  } elseif( !preg_match( '/^[0-9a-z_.\/?-]+@([0-9a-z-]+\.)+[0-9a-z-]+$/', $data['email']) ) {
-		$error[] = "「メールアドレス」は正しい形式で入力してください。";
-	}
+  if (!empty($clean['btn_confirm'])) {
+    $error = validation($clean);
 
-  // お問い合わせ内容のバリデーション
-	if( empty($data['contact']) ) {
-		$error[] = "「お問い合わせ内容」は必ず入力してください。";
-	}
+    if (empty($error)) {
+      $page_flag = 1;
 
-  return $error;
-}
+      // セッションの書き込み
+      session_start();
+      $_SESSION['page'] = true;
+    }
+  } elseif (!empty($clean['btn_submit'])) {
+    session_start();
+
+    if (!empty($_SESSION['page']) && $_SESSION['page'] === true) {
+      // セッションの削除
+      unset($_SESSION['page']);
+      $page_flag = 2;
+
+      // 変数とタイムゾーンを初期化
+      $header = null;
+      $auto_reply_subject = null;
+      $auto_reply_text = null;
+      $admin_reply_subject = null;
+      $admin_reply_text = null;
+      date_default_timezone_set('Asia/Tokyo');
+
+      // ヘッダー情報を設定
+      $header = "MIME-Version: 1.0\n";
+      $header .= "From: 株式会社前野企画 <maenokikaku@email.plala.or.jp>\n";
+      $header .= "Reply-To: 株式会社前野企画 <maenokikaku@email.plala.or.jp>\n";
+
+      // 件名を設定
+      $auto_reply_subject = 'お問い合わせありがとうございます。';
+
+      // 本文を設定
+      $auto_reply_text = "この度は、お問い合わせ頂き誠にありがとうございます。
+    下記の内容でお問い合わせを受け付けました。\n\n";
+      $auto_reply_text .= "お問い合わせ日時：" . date("Y-m-d H:i") . "\n";
+      $auto_reply_text .= "氏名：" . $clean['your_name'] . "\n";
+      $auto_reply_text .= "メールアドレス：" . $clean['email'] . "\n\n";
+      $auto_reply_text .= "電話番号：" . $clean['tel'] . "\n\n";
+      $auto_reply_text .= "お問い合わせ内容：" . nl2br($clean['contact']) . "\n\n";
+      $auto_reply_text .= "株式会社前野企画";
+
+      // メール送信
+      mb_send_mail($clean['email'], $auto_reply_subject, $auto_reply_text, $header);
+
+      // 運営側へ送るメールの件名
+      $admin_reply_subject = "お問い合わせを受け付けました";
+
+      // 本文を設定
+      $admin_reply_text = "下記の内容でお問い合わせがありました。\n\n";
+      $admin_reply_text .= "お問い合わせ日時：" . date("Y-m-d H:i") . "\n";
+      $admin_reply_text .= "氏名：" . $clean['your_name'] . "\n";
+      $admin_reply_text .= "メールアドレス：" . $clean['email'] . "\n\n";
+      $admin_reply_text .= "電話番号：" . $clean['tel'] . "\n\n";
+      $admin_reply_text .= "お問い合わせ内容：" . nl2br($clean['contact']) . "\n\n";
+
+      // 運営側へメール送信
+      mb_send_mail('maenokikaku@email.plala.or.jp', $admin_reply_subject, $admin_reply_text, $header);
+    } else {
+      $page_flag = 0;
+    }
+  }
+
+  function validation($data) {
+    $error = array();
+
+    // 氏名のバリデーション
+    if (empty($data['your_name'])) {
+      $error[] = "「氏名」は必ず入力してください。";
+    } elseif (20 < mb_strlen($data['your_name'])) {
+      $error[] = "「氏名」は20文字以内で入力してください。";
+    }
+
+    // メールアドレスのバリデーション
+    if (empty($data['email'])) {
+      $error[] = "「メールアドレス」は必ず入力してください。";
+    } elseif (!preg_match( '/^[0-9a-z_.\/?-]+@([0-9a-z-]+\.)+[0-9a-z-]+$/', $data['email'])) {
+      $error[] = "「メールアドレス」は正しい形式で入力してください。";
+    }
+
+    // お問い合わせ内容のバリデーション
+    if (empty($data['contact'])) {
+      $error[] = "「お問い合わせ内容」は必ず入力してください。";
+    }
+
+    return $error;
+  }
 ?>
 
 <html lang="ja">
