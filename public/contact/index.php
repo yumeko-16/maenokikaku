afe2f71119872d0828e10da7f1f0b79fee39abf7
tomohiro.maeno <>
2025-09-28 01:01:07 +0700

    clean: Reformat.

--- public/contact/index.php
+++ public/contact/index.php
@@ -192,10 +192,26 @@
       })(document);
     </script>
 
-    <script type="module" crossorigin src="/assets/modulepreload-polyfill.js"></script>
-    <script type="module" crossorigin src="/assets/scripts/common.js"></script>
-    <script type="module" crossorigin src="/assets/scripts/pages/contact/index.js"></script>
-    <link rel="stylesheet" crossorigin href="/assets/styles/pages/contact/style.css">
+    <script
+      type="module"
+      crossorigin
+      src="/assets/modulepreload-polyfill.js"
+    ></script>
+    <script
+      type="module"
+      crossorigin
+      src="/assets/scripts/common.js"
+    ></script>
+    <script
+      type="module"
+      crossorigin
+      src="/assets/scripts/pages/contact/index.js"
+    ></script>
+    <link
+      rel="stylesheet"
+      crossorigin
+      href="/assets/styles/pages/contact/style.css"
+    />
   </head>
 
   <body>
